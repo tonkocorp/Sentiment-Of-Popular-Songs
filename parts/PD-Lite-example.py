fd02051e1b98b8845a76f7c83a8ee22722b58a22
tonkocorp <46730210+tonkocorp@users.noreply.github.com>
2021-04-27 17:57:41 -0500

    started dash

--- parts/PD-Lite-example.py
+++
@@ -1,7 +0,0 @@
-import sqlite3 as sql
-import pandas as pd
-import matplotlib.pyplot as plt
-
-weather = pd.read_csv(
-    'https://github.com/alanjones2/dataviz/raw/master/londonweather.csv')
-    
